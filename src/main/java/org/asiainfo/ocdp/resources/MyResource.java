2d1d09c9b9569b5efff5bbdee386065e1b67bb9c
yuls3 <yulishan3@gmail.com>
2018-04-16 10:46:08 +0800

    init project

---
+++ src/main/java/org/asiainfo/ocdp/resources/MyResource.java
@@ -0,0 +1,42 @@
+package org.asiainfo.ocdp.resources;
+
+import io.swagger.annotations.*;
+import org.asiainfo.ocdp.bean.MyBean;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+@Api(value = "/myresource", description = "My general resources test")
+public class MyResource {
+
+  /**
+   * Method handling HTTP GET requests. The returned object will be sent
+   * to the client as "text/plain" media type.
+   *
+   * @return String that will be returned as a text/plain response.
+   */
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String getIt() {
+    return "Got it!";
+  }
+
+  @GET
+  @Path("/test")
+  @ApiOperation(value = "Get a POJO model")
+  @ApiResponses(value = {
+    @ApiResponse(code = 400, message = "Invalid ID supplied",
+      responseHeaders = @ResponseHeader(name = "X-Rack-Cache", description = "Explains whether or not a cache was used", response = Boolean.class)),
+    @ApiResponse(code = 404, message = "Pet not found")})
+  @Produces(MediaType.APPLICATION_JSON)
+  public MyBean getTest() {
+    return new MyBean("Yu", "30");
+  }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Jersey RESTful Web Application!</h2>
+<p><a href="/api/v1/myresource">Jersey resource</a>
+<p>Visit <a href="/swagger">Api documentations</a>
+  for more api information!
+</body>
+</html>
