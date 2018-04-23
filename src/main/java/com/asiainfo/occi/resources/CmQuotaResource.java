ffff8a2babf592a9fac02c357fff162303066ac2
yuls3 <yulishan3@gmail.com>
2018-04-23 14:35:35 +0800

    Fix api docs issue

--- src/main/java/com/asiainfo/occi/resources/CmQuotaResource.java
+++ src/main/java/com/asiainfo/occi/resources/CmQuotaResource.java
@@ -1,10 +1,9 @@
 package com.asiainfo.occi.resources;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -25,8 +24,7 @@ public class CmQuotaResource {
 	@GET
 	@Path("/hdfs")
 	@Produces((MediaType.APPLICATION_JSON + ";charset=utf-8"))
-	public Response getHdfsQuota(@Context HttpServletRequest request) {
-		String path = request.getParameter("path");
+	public Response getHdfsQuota(@QueryParam("path") String path) {
 		long storage = HDFSClient.getInstance().getTotalStorage(path);
 		long namespace = HDFSClient.getInstance().getTotalNamespace(path);
 		long usedStorage = HDFSClient.getInstance().getUsedStorge(path);
