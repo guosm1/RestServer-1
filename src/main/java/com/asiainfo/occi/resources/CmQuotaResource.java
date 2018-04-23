5a45fb858a6b66c2d79cfc3426bbc1dba1642cf9
autumn1224@163.com <autumn1224@163.com>
2018-04-23 14:25:21 +0800

    update hdfs client

--- src/main/java/com/asiainfo/occi/resources/CmQuotaResource.java
+++ src/main/java/com/asiainfo/occi/resources/CmQuotaResource.java
@@ -11,10 +11,6 @@ import javax.ws.rs.core.Response;
 import com.asiainfo.occi.client.impl.HDFSClient;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.ResponseHeader;
 
 /**
  * 
@@ -28,10 +24,6 @@ public class CmQuotaResource {
 
 	@GET
 	@Path("/hdfs")
-	@ApiOperation(value = "Get hdfs quota info")
-	@ApiResponses(value = {
-			@ApiResponse(code = 400, message = "Bad request", responseHeaders = @ResponseHeader(name = "X-Rack-Cache", description = "Explains whether or not a cache was used", response = Boolean.class)),
-			@ApiResponse(code = 404, message = "Page not found") })
 	@Produces((MediaType.APPLICATION_JSON + ";charset=utf-8"))
 	public Response getHdfsQuota(@Context HttpServletRequest request) {
 		String path = request.getParameter("path");
@@ -39,23 +31,11 @@ public class CmQuotaResource {
 		long namespace = HDFSClient.getInstance().getTotalNamespace(path);
 		long usedStorage = HDFSClient.getInstance().getUsedStorge(path);
 		long usedNs = HDFSClient.getInstance().getUsedNamespace(path);
-		return Response.ok().entity("{"+
-			  "\"items\": [{"+
-				    "\"available\": \"xxx\","+
-				    "\"name\": \"nameSpaceQuota\","+
-				    "\"resource\": \"" + path+ "\"," +
-				    "\"size\": \"" + namespace +"\","+
-				    "\"used\": \"" + usedNs + "\"" +
-				  "}, {"+
-				    "\"available\": \"xxx\","+
-				    "\"name\": \"storageSpaceQuota\","+
-				    "\"resource\": \"" + path + "\","+
-				    "\"size\": \"" + storage +"\","+
-				    "\"used\": \"" + usedStorage +"\""+
-				  "}]"+
-				"}").build();
+		return Response.ok().entity("{" + "\"items\": [{" + "\"available\": \"xxx\"," + "\"name\": \"nameSpaceQuota\","
+				+ "\"resource\": \"" + path + "\"," + "\"size\": \"" + namespace + "\"," + "\"used\": \"" + usedNs
+				+ "\"" + "}, {" + "\"available\": \"xxx\"," + "\"name\": \"storageSpaceQuota\"," + "\"resource\": \""
+				+ path + "\"," + "\"size\": \"" + storage + "\"," + "\"used\": \"" + usedStorage + "\"" + "}]" + "}")
+				.build();
 	}
-		
-
 
 }
